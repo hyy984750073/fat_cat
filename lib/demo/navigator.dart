196101d8f47a9285c8041dbbcfbd2f25ae85da7d
hyy984750073 <984750073@qq.com>
2022-11-14 11:11:41 +0800

    普通路由传参

--- lib/demo/navigator.dart
+++ lib/demo/navigator.dart
@@ -1,24 +1,56 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class NavigatorDemo extends StatelessWidget {
+// class NavigatorDemo extends StatelessWidget {
+//   final String id;
+// const NavigatorDemo({ Key? key, required this.id }) : super(key: key);
+// // const NavigatorDemo({ Key? key, this.id = 'init id' }) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context){
+//     // dynamic arguments = ModalRoute.of(context).settings.arguments;
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('点击页面')
+//       ),
+//       body: CupertinoButton(
+//         child: Text('可以点击的按钮1111111'),
+//         onPressed: () {
+//           print(this.id);
+//           print('被点击啦呀');
+//         }
+//       )
+//     );
+//   }
+// }
+
+class NavigatorDemo extends StatefulWidget {
   final String id;
-const NavigatorDemo({ Key? key, required this.id }) : super(key: key);
+  final int age;
+  // 这里如果是const的话，上面定义的得是final
+  // required必须排在前面
+  // const NavigatorDemo({super.key, this.id = 'init id'});
+  const NavigatorDemo({super.key, required this.id, this.age = 14});
 
   @override
-  Widget build(BuildContext context){
-    // dynamic arguments = ModalRoute.of(context).settings.arguments;
+  State<NavigatorDemo> createState() => _NavigatorDemoState();
+}
+
+class _NavigatorDemoState extends State<NavigatorDemo> {
+  @override
+  void initState() {
+    super.initState();
+    print(widget.age);
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('点击页面')
-      ),
-      body: CupertinoButton(
-        child: Text('可以点击的按钮1111111'),
-        onPressed: () {
-          print(this.id);
-          print('被点击啦呀');
-        }
-      )
-    );
+        appBar: AppBar(title: Text(widget.id)),
+        body: CupertinoButton(
+            child: Text('可以点击的按钮1111111'),
+            onPressed: () {
+              print('被点击啦呀');
+            }));
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -20,78 +20,78 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue
       ),
-      // home: const MyHomePage(title: 'Flutter Demo Home Page'),
-      routes: routes,
-      home: const Login(),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      // routes: routes,
+      // home: const Login(),
       // 路由钩子，可以对指定路由进行拦截，有时候不想改变页面结构但是又想要求跳转到这个页面的时候可以用到，比如页面设定了传参你进行跳转的时候
       // onGenerateRoute: onGenerateRoute,
     );
   }
 }
 
-// class MyHomePage extends StatefulWidget {
-//   const MyHomePage({super.key, required this.title});
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
 
-//   final String title;
-//   // 状态
-//   @override
-//   State<MyHomePage> createState() => _MyHomePageState();
-// }
+  final String title;
+  // 状态
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
 
-// class _MyHomePageState extends State<MyHomePage> {
-//   int _counter = 0;
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
 
-//   void _incrementCounter() {
-//     setState(() {
-//       _counter++;
-//     });
-//   }
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
 
-//   @override
-//   Widget build(BuildContext context) {
-//     // 头部左右边的导航
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text(widget.title),
-//         centerTitle: true
-//       ),
-//       body: Center(
-//         child: Column(
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           children: <Widget>[
-//             const Text(
-//               'You have pushed the button this many times:',
-//             ),
-//             Text(
-//               '$_counter',
-//               style: Theme.of(context).textTheme.headline4,
-//             ),
-//             // 自己写的点击事件
-//             Trigger(),
-//             CupertinoButton(
-//               child: Text('可以点击的按钮'),
-//               onPressed: () {
-//                 print('被点击啦');
-//                 Navigator.of(context).push(MaterialPageRoute(
-//                   builder: (context) {
-//                     // 对应页面 路由传参
-//                     return NavigatorDemo(id: '1234');
-//                   },
-//                   settings: RouteSettings(
-//                     name: 'menu',
-//                     arguments: {'name': 'Lisa'}
-//                   ),
-//                 )).then((value) => print(value));
-//               }
-//             ),
-//           ],
-//         ),
-//       ),
-//       floatingActionButton: FloatingActionButton(
-//         onPressed: _incrementCounter,
-//         tooltip: 'Increment',
-//         child: const Icon(Icons.add),
-//       ),
-//     );
-//   }
-// }
+  @override
+  Widget build(BuildContext context) {
+    // 头部左右边的导航
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+        centerTitle: true
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+            // 自己写的点击事件
+            Trigger(),
+            CupertinoButton(
+              child: Text('可以点击的按钮'),
+              onPressed: () {
+                print('被点击啦');
+                Navigator.of(context).push(MaterialPageRoute(
+                  builder: (context) {
+                    // 对应页面 路由传参
+                    return NavigatorDemo(id: '我是', age: 15);
+                  },
+                  settings: RouteSettings(
+                    name: 'menu',
+                    arguments: {'name': 'Lisa'}
+                  ),
+                )).then((value) => print(value));
+              }
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
