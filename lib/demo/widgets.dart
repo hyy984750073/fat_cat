43d9690a62ca2cc9fb46346adb4ff62a1e55b4dd
hyy984750073 <984750073@qq.com>
2022-11-23 17:34:57 +0800

    完善表单更多功能

--- lib/demo/widgets.dart
+++ lib/demo/widgets.dart
@@ -1,8 +1,16 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class WidgetsDemoPage extends StatelessWidget {
+class WidgetsDemoPage extends StatefulWidget {
   const WidgetsDemoPage({super.key});
 
+  @override
+  State<WidgetsDemoPage> createState() => _WidgetsDemoPageState();
+}
+
+class _WidgetsDemoPageState extends State<WidgetsDemoPage> {
+  bool _check = true;
+  bool _switch = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,9 +31,11 @@ class WidgetsDemoPage extends StatelessWidget {
             borderRadius: const BorderRadius.all(Radius.circular(8)),
           ),
           child: Column(
+              // mainAxisSize: MainAxisSize.min,
               mainAxisAlignment: MainAxisAlignment.start, // 主轴对齐方式,Column的主轴为竖轴
               crossAxisAlignment: CrossAxisAlignment.center, // 交叉轴
               children: [
+                // 居中
                 const Center(
                   child: Text(
                     'Hello World',
@@ -40,37 +50,80 @@ class WidgetsDemoPage extends StatelessWidget {
                   ),
                 ),
                 Row(
-                  // 主轴对齐方式,Row的主轴为横轴
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: const [
-                    Icon(
-                      Icons.star,
-                      color: Colors.yellow,
+                  children: [
+                    // 复选框
+                    Checkbox(
+                        value: _check,
+                        onChanged: (val) {
+                          setState(() {
+                            _check = val!;
+                          });
+                        }),
+                    // 开关
+                    Switch(
+                        value: _switch,
+                        onChanged: (val) {
+                          setState(() {
+                            _switch = val;
+                          });
+                        }),
+                    // 网络图片
+                    Image.network(
+                      'https://bkimg.cdn.bcebos.com/pic/f636afc379310a55b319e1d3600c54a98226cffcbac9?x-bce-process=image/watermark,image_d2F0ZXIvYmFpa2U5Mg==,g_7,xp_5,yp_5',
+                      width: 70,
                     ),
-                    Icon(
-                      Icons.star,
-                      color: Colors.green,
-                    ),
-                    Icon(
-                      Icons.star,
-                      color: Colors.red,
-                    ),
+                    // Icon按钮
+                    IconButton(onPressed: () {}, icon: const Icon(Icons.add)),
                   ],
                 ),
+                const SizedBox(height: 8),
+                // 进度指示器 横线
+                const LinearProgressIndicator(
+                  value: .8,
+                  valueColor: AlwaysStoppedAnimation(Colors.red),
+                ),
+                const SizedBox(height: 8),
+                // 进度指示器 环形
+                const CircularProgressIndicator(
+                  value: .8,
+                  valueColor: AlwaysStoppedAnimation(Colors.red),
+                ),
+                // loading  IOS风格
+                const CupertinoActivityIndicator(),
+
+
+                InkWell(
+                  splashColor: Colors.blue.withAlpha(30),
+                  onTap: () {
+                    debugPrint('Card tapped.');
+                  },
+                  child: const SizedBox(
+                    width: 300,
+                    height: 20,
+                    child: Text('A card that can be tapped'),
+                  ),
+                ),
+                // 水平方向列表
                 Row(
-                  // 主轴对齐方式,Row的主轴为横轴
+                  // 主轴对齐方式,Row的主轴为横轴  MainAxisAlignment为枚举类
                   mainAxisAlignment: MainAxisAlignment.start,
                   children: [
                     Expanded(
-                      child: Image.asset('assets/images/eye.png', fit: BoxFit.none,),
+                      child: Image.asset('assets/images/eye.png',
+                          fit: BoxFit.contain, // 图片填充模式
+                          color: Colors.red,
+                          height: 50),
                     ),
                     Expanded(
                       flex: 2,
-                      child: Image.asset('assets/images/eye.png'),
+                      child: Image.asset('assets/images/eye.png', height: 50),
                     ),
                     Expanded(
                       flex: 3,
-                      child: Image.asset('assets/images/eye.png'),
+                      child: Image.asset(
+                        'assets/images/eye.png',
+                        height: 50,
+                      ),
                     ),
                   ],
                 ),
@@ -83,31 +136,31 @@ class WidgetsDemoPage extends StatelessWidget {
                         width: 120,
                         child: Text('123456'),
                       ),
-                      Image.asset('assets/images/eye.png'),
+                      Image.asset('assets/images/eye.png', height: 60),
                     ],
                   ),
                 ),
                 // ListTile
-                ListTile(
-                  title: const Text(
+                const ListTile(
+                  title: Text(
                     '1625 Main Street',
                     style: TextStyle(fontWeight: FontWeight.w500),
                   ),
-                  subtitle: const Text('My City, CA 99984'),
+                  subtitle: Text('My City, CA 99984'),
                   leading: Icon(
                     Icons.restaurant_menu,
-                    color: Colors.blue[500],
+                    color: Colors.red,
                   ),
                 ),
                 const Divider(),
-                ListTile(
-                  title: const Text(
+                const ListTile(
+                  title: Text(
                     '(408) 555-1212',
                     style: TextStyle(fontWeight: FontWeight.w500),
                   ),
                   leading: Icon(
                     Icons.contact_phone,
-                    color: Colors.blue[500],
+                    color: Color(0xffC9BA9B),
                   ),
                 ),
                 ListTile(
--- lib/user/login.dart
+++ lib/user/login.dart
@@ -8,7 +8,34 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
+  // <FormState>  泛型   表单的状态
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  // TextEditingController就是在OC中UITextFeild 控件 [TextEditingController.addListener].监听输入框输入字符 类似于UITextFeild的textvalueDidChange代理
+  final TextEditingController _controllerPassword = TextEditingController();
+  final TextEditingController _controllerUser = TextEditingController();
+  final FocusNode _userNode = FocusNode();
+  final FocusNode _passwordNode = FocusNode();
+  // FocusScopeNode _focusScopeNode;
+
+  // 进来时
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
+  // 退出时
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    // 退出时注释掉那些很占用内存的东西，否则占用内存
+    _controllerPassword.dispose();
+    _controllerUser.dispose();
+    _userNode.dispose();
+    _passwordNode.dispose();
+    // if (_focusScopeNode != null) _focusScopeNode.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -33,25 +60,74 @@ class _LoginState extends State<Login> {
             child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
+                  // 表单输入框，是可以校验的
                   TextFormField(
+                    autofocus: true, // 自动获取焦点
+                    // 捕捉监听TextField的焦点获取与失去，同时也可通过FocusNode来使用绑定对应的TextField获取焦点与失去焦点
+                    focusNode: _userNode,
+                    controller: _controllerUser,
                     decoration: const InputDecoration(
                       hintText: '账号',
+                      prefixIcon: Icon(Icons.usb_rounded), // 框内
                     ),
                     validator: (String? value) {
                       if (value == null || value.isEmpty) {
                         return '请输入您的手机号/邮箱';
+                      } else if (value.length < 8) {
+                        return '长度大于8';
                       }
                       return null;
+                    },
+                    // 输入类型：搜索类型，则输入法有搜索
+                    textInputAction: TextInputAction.next,
+                    // 当我们按回车时
+                    onFieldSubmitted: (val) {
+                      print('ssd');
+                    },
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  // 单纯输入框
+                  TextField(
+                    controller: _controllerPassword,
+                    obscureText: true, // 是否为密码框
+                    focusNode: _passwordNode,
+                    decoration: const InputDecoration(
+                      // icon: Icon(Icons.unsubscribe_outlined), // 框外
+                      // border: OutlineInputBorder(), // 是否有边框
+                      prefixIcon: Icon(Icons.unsubscribe_outlined), // 框内
+                      labelText: '密码',
+                      hintText: '默认文字',
+                    ),
+                    onChanged: (val) {
+                      // print(val);
                     },
                   ),
                   Padding(
                     padding: const EdgeInsets.symmetric(vertical: 16.0),
                     child: ElevatedButton(
                       onPressed: () {
-                        // Validate will return true if the form is valid, or false if
-                        // the form is invalid.
+                        print('密码${_controllerPassword.text}');
+                        print('账号${_controllerUser.text}');
+                        print(_formKey.currentState!.validate().toString());
+
+                        // // 为空的时候给它赋值
+                        // if (_focusScopeNode == null) {
+                        //   _focusScopeNode = FocusScope.of(context);
+                        // }
+                        // // 有值之后,将光标给到‘账号’
+                        // _focusScopeNode.requestFocus(_userNode);
+                        // // 不想要焦点,可以让界面所有的焦点消失
+                        // _focusScopeNode.unfocus();
+
+                        /**
+                         * (_formKey.currentState as FormState).validate()
+                         * 可以写成
+                         * _formKey.currentState!.validate()
+                        */
+                        // if ((_formKey.currentState as FormState).validate()) {
                         if (_formKey.currentState!.validate()) {
-                          // Process data.
                           Navigator.of(context).pushNamedAndRemoveUntil(
                               '/tabs', (route) => false);
                         }
@@ -64,7 +140,7 @@ class _LoginState extends State<Login> {
                       var toRegister = Navigator.pushNamed(context, "/register",
                           arguments: {'phone': '15278061180', 'age': 15});
 
-                          toRegister.then((value) => print(value));
+                      toRegister.then((value) => print(value));
                     },
                     child: const Text('去注册'),
                   ),
